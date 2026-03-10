a1133435d4aed30ef90024f66320d5812dbcf233
Bhanu <myemail.bhanu@gmail.com>
2026-03-10 11:56:26 +0530

    putting up a wip page!

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-gsap.from("h1", { opacity: 0, y: 50, duration: 1, ease: "elastic.out(1, 0.5)" })
+document.fonts.ready.then(() => {
+  document.documentElement.style.visibility = 'visible'
+})
+
+gsap.from("h1, body", { 
+  opacity: 0, 
+  y: 50, 
+  duration: 1, 
+  
+  ease: "elastic.out(1, 0.5)" 
+});
